37fd6381b1968a146e885557a0758e9b2d7df85c
Maxim Kokosha <kokosha.maxim@gmail.com>
2020-03-30 15:47:55 +0300

    feat(auth) add auth API method

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const secretKey = config.get('jtwSecretKey');
 
-router.post('/', (req, res) => {
-  console.log('req:', req.body);
-  res.send('auth');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Please, provide valid email')
+      .isEmail()
+      .normalizeEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+      return res.status(400).json({ errors: validationErrors.array() });
+    }
+
+    try {
+      const { email, password } = req.body;
+      let user = await User.findOne({ email });
+      const isUserNotExist = user == null;
+
+      if (isUserNotExist) {
+        return res
+          .status(400)
+          .json({ errors: [{ message: 'Credentials invalid' }] });
+      }
+
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ message: 'Credentials invalid' }] });
+      }
+
+      const jwtPayload = { user: { id: user.id } };
+
+      jwt.sign(jwtPayload, secretKey, { expiresIn: '1d' }, (err, token) => {
+        if (err) throw err;
+
+        res.status(200).json({ token });
+      });
+    } catch (error) {
+      res.status(500, `Internal Server Error`);
+    }
+  }
+);
 
 module.exports = router;
