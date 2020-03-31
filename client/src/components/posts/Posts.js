b29beac00c4f4f5eb592ed13c53c28cfea408f1c
Maxim Kokosha <kokosha.maxim@gmail.com>
2020-03-31 15:00:01 +0300

    client(posts) add hook for posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Posts = () => {
-  return <div>POsts...</div>;
+import { getPosts } from '../../actions';
+
+const Posts = ({ getPosts, posts }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  return <div></div>;
 };
 
-export default Posts;
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  posts: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  posts: state.posts
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
